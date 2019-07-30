2d7f4844ac3e06357b34f805c62544234fc425ba
Chevady <35552608+sherlock666@users.noreply.github.com>
2019-07-30 23:43:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -82,22 +82,6 @@ def get_answer(message_text):
     except Exception:
         return "Error occurs when finding answer"
 
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
     
 # 處理訊息
 
