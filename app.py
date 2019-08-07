9c66500266516d4ee11fdfe8bfc69209c4945c52
Chevady <35552608+sherlock666@users.noreply.github.com>
2019-08-07 23:09:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -295,8 +295,8 @@ def handle_message(event):
                     )
                 )
 
-                line_bot_api.reply_message(event.reply_token, carousel_template_message)
-                return 0
+            line_bot_api.reply_message(event.reply_token, carousel_template_message)
+            return 0
 
 
 
