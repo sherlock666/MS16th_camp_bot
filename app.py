3a1744383fe19b07c21f23a81b070606c159d663
Chevady <35552608+sherlock666@users.noreply.github.com>
2019-08-10 13:59:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ def handle_message(event):
 
         if event.message.text == "領袖營":
             camp_menu_messages = ImagemapSendMessage(
-                base_url='https://raw.githubusercontent.com/sherlock666/LineBot-QNAmaker/master/button_template/16th_camp_menu.png',
+                base_url='https://i.imgur.com/j7WRnJ9.png',
                 alt_text='this is an imagemap',
                 base_size=BaseSize(width=1040, height=1040),
                 actions=[
