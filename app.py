662cae62e050bc6341b45bab8cacb69186cbd77f
Chevady <35552608+sherlock666@users.noreply.github.com>
2019-08-10 15:11:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ def handle_message(event):
 
         if event.message.text == "領袖營":
             camp_menu_messages = ImagemapSendMessage(
-                base_url='https://i.imgur.com/xqM8bvM.png',
+                base_url='https://i.imgur.com/xqM8bvM/700',
                 alt_text='this is an imagemap',
                 base_size=BaseSize(width=1040, height=1040),
                 actions=[
